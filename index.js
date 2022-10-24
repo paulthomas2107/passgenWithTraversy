1da9c39b9d5e2edb6fa70c6848a8e6d78ef0b161
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 20:56:15 +0100

    More...

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const log = console.log
 const program = require('commander')
+const chalk = require("chalk");
+const clipboardy = require('node-clipboardy')
 const createPassword = require('./utils/createPassword')
+const savePassword = require('./utils/savePassword')
+
 
 program.version('1.0.0').description("Simple Password Generator")
 
@@ -16,4 +20,14 @@ const {length, save, numbers, symbols} = program.opts()
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols)
 
-log(generatedPassword)
+// Save to file
+if (save) {
+    savePassword(generatedPassword)
+}
+
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword)
+
+log(chalk.blue("Generated password: " + chalk.bold(generatedPassword)))
+log(chalk.yellow('Password copied to clipboard'))
+
