939b3f3457fb6a4d77fcdbc7fd8f1f35b8a01f07
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 19:26:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const program = require('commander')
+
+program.version('1.0.0').description("Simple Password Generator")
+
+program.command('generate').action(() => {
+    console.log('Generated')
+}).parse()
+
+
